88c8e5e22de415b3693535af1aa9259748023fe8
xrachitx <45661647+xrachitx@users.noreply.github.com>
2022-01-06 23:48:05 +0530

    Update sklarge_flux.py

--- datasets/sklarge_flux.py
+++ datasets/sklarge_flux.py
@@ -7,6 +7,58 @@ import torch
 import os
 from torch.utils.data import Dataset, DataLoader
 
+def loadsklarge(self, rootDir, frame, imgidx, gtidx):
+    # load image and skeleton
+    inputName = os.path.join(rootDir, frame.iloc[imgidx, 0])
+    targetName = os.path.join(rootDir, frame.iloc[gtidx, 1])
+    image = cv2.imread(inputName, 1)
+    skeleton = cv2.imread(targetName, 0)
+    print("IMAGE: ", image.shape, "skeleton: ", skeleton.shape)
+    skeleton = (skeleton > 0).astype(np.uint8)
+    image = image.astype(np.float32)
+    image -= self.mean
+    image = image.transpose((2, 0, 1))
+
+    # compute flux and dilmask
+    kernel = np.ones((15, 15), np.uint8)
+    dilmask = cv2.dilate(skeleton, kernel)
+    rev = 1 - skeleton
+    height = rev.shape[0]
+    width = rev.shape[1]
+    rev = (rev > 0).astype(np.uint8)
+    dst, labels = cv2.distanceTransformWithLabels(rev, cv2.DIST_L2, cv2.DIST_MASK_PRECISE,
+                                                  labelType=cv2.DIST_LABEL_PIXEL)
+
+    index = np.copy(labels)
+    index[rev > 0] = 0
+    place = np.argwhere(index > 0)
+
+    nearCord = place[labels - 1, :]
+    x = nearCord[:, :, 0]
+    y = nearCord[:, :, 1]
+    nearPixel = np.zeros((2, height, width))
+    nearPixel[0, :, :] = x
+    nearPixel[1, :, :] = y
+    grid = np.indices(rev.shape)
+    grid = grid.astype(float)
+    diff = grid - nearPixel
+
+    dist = np.sqrt(np.sum(diff ** 2, axis=0))
+
+    direction = np.zeros((2, height, width), dtype=np.float32)
+    direction[0, rev > 0] = np.divide(diff[0, rev > 0], dist[rev > 0])
+    direction[1, rev > 0] = np.divide(diff[1, rev > 0], dist[rev > 0])
+
+    direction[0] = direction[0] * (dilmask > 0)
+    direction[1] = direction[1] * (dilmask > 0)
+
+    flux = -1 * np.stack((direction[0], direction[1]))
+
+    dilmask = (dilmask > 0).astype(np.float32)
+    dilmask = dilmask[np.newaxis, ...]
+    print(image.shape,flux.shape,dilmask.shape)
+    return image, flux, dilmask
+
 
 def _collate_fn(batch):
     
@@ -17,38 +69,50 @@ def _collate_fn(batch):
     dilmasks = torch.zeros(minibatch_size, 1, size_x, size_y)
     for x in range(minibatch_size):
         sample = batch[x]
-        tensor = sample[0]
-        target = sample[1]
-        w_a,h_a = tensor.shape[1], tensor.shape[2]
-        maxx = max(w_a,h_a)
-        cap = 256
-        extra = 0
+        rootDir = sample[0]
+        frame = sample[1]
+        idx = sample[2]
+        inputName = os.path.join(rootDir, frame.iloc[idx, 0])
+        targetName = os.path.join(rootDir, frame.iloc[ifx, 1])
+        tensor = cv2.imread(inputName, 1)
+        target = cv2.imread(targetName, 0)        
+        tensor = torch.Tensor(tensor)
+        target = torch.Tensor(target)
+        print("tensor shape: ", tensor, "target shape: ", target.shape)
+        
+#         tensor = tensor.transpose((2, 0, 1))
+        
+#         w_a,h_a = tensor.shape[1], tensor.shape[2]
+#         maxx = max(w_a,h_a)
+#         cap = 256
+#         extra = 0
 
-        if maxx<= cap:
-            new_w, new_h = w_a,h_a
-        else:
-            r = cap/maxx
-            new_w, new_h = int(w_a*r), int(h_a*r)
-            img_transform = transforms.Compose([transforms.ToPILImage(mode="RGB"),transforms.Resize((new_h, new_w)), transforms.ToTensor()])
-            gt_transform = transforms.Compose([transforms.ToPILImage(mode="L"),transforms.Resize((new_h, new_w)), transforms.ToTensor()])
-            tensor = img_transform(tensor)
-            target = gt_transform(target)
+#         if maxx<= cap:
+#             new_w, new_h = w_a,h_a
+#         else:
+#             r = cap/maxx
+#             new_w, new_h = int(w_a*r), int(h_a*r)
+#             img_transform = transforms.Compose([transforms.ToPILImage(mode="RGB"),transforms.Resize((new_h, new_w)), transforms.ToTensor()])
+#             gt_transform = transforms.Compose([transforms.ToPILImage(mode="L"),transforms.Resize((new_h, new_w)), transforms.ToTensor()])
+#             tensor = img_transform(tensor)
+#             target = gt_transform(target)
 
-        p_up = (cap+extra-new_h)//2
-        p_down = cap+extra-p_up-new_h
+#         p_up = (cap+extra-new_h)//2
+#         p_down = cap+extra-p_up-new_h
 
-        p_left = (cap+extra-new_w)//2
-        p_right = cap+extra-p_left-new_w
+#         p_left = (cap+extra-new_w)//2
+#         p_right = cap+extra-p_left-new_w
             
-        tensor = torch.nn.functional.pad(tensor,(p_left,p_right,p_up,p_down),value=0)
-        target = torch.nn.functional.pad(target,(p_left,p_right,p_up,p_down),value=0)
-        target = torch.squeeze(target,axis=0)
-        imgs[x,:,:,:] = tensor
-        gts[x,:,:] = target
+#         tensor = torch.nn.functional.pad(tensor,(p_left,p_right,p_up,p_down),value=0)
+#         target = torch.nn.functional.pad(target,(p_left,p_right,p_up,p_down),value=0)
+#         target = torch.squeeze(target,axis=0)
+        
+# #         imgs[x,:,:,:] = tensor
+# #         gts[x,:,:] = target
         
     
-#     targets = torch.IntTensor(targets)
-    return imgs, gts
+# #     targets = torch.IntTensor(targets)
+#     return imgs, gts
 
 class DataLayer(Dataset):
 
@@ -69,61 +133,15 @@ class DataLayer(Dataset):
     def __getitem__(self, idx):
         # load image, flux and dilmask
         
-        self.image, self.flux, self.dilmask = self.loadsklarge(idx,
-                                                               idx)
-        return self.image, self.flux, self.dilmask
+#         self.image, self.flux, self.dilmask = self.loadsklarge(self.rootDir, self.frame, idx,
+#                                                                idx)
+        return self.rootDir, self.frame, idx
 
-    def loadsklarge(self, imgidx, gtidx):
-        # load image and skeleton
-        inputName = os.path.join(self.rootDir, self.frame.iloc[imgidx, 0])
-        targetName = os.path.join(self.rootDir, self.frame.iloc[gtidx, 1])
-        image = cv2.imread(inputName, 1)
-        skeleton = cv2.imread(targetName, 0)
-        print("IMAGE: ", image.shape, "skeleton: ", skeleton.shape)
-        skeleton = (skeleton > 0).astype(np.uint8)
-        image = image.astype(np.float32)
-        image -= self.mean
-        image = image.transpose((2, 0, 1))
+class ImageDataLoader(DataLoader):
+    def __init__(self, *args, **kwargs):
+        super(ImageDataLoader, self).__init__(*args, **kwargs)
+        self.collate_fn = _collate_fn
 
-        # compute flux and dilmask
-        kernel = np.ones((15, 15), np.uint8)
-        dilmask = cv2.dilate(skeleton, kernel)
-        rev = 1 - skeleton
-        height = rev.shape[0]
-        width = rev.shape[1]
-        rev = (rev > 0).astype(np.uint8)
-        dst, labels = cv2.distanceTransformWithLabels(rev, cv2.DIST_L2, cv2.DIST_MASK_PRECISE,
-                                                      labelType=cv2.DIST_LABEL_PIXEL)
-
-        index = np.copy(labels)
-        index[rev > 0] = 0
-        place = np.argwhere(index > 0)
-
-        nearCord = place[labels - 1, :]
-        x = nearCord[:, :, 0]
-        y = nearCord[:, :, 1]
-        nearPixel = np.zeros((2, height, width))
-        nearPixel[0, :, :] = x
-        nearPixel[1, :, :] = y
-        grid = np.indices(rev.shape)
-        grid = grid.astype(float)
-        diff = grid - nearPixel
-
-        dist = np.sqrt(np.sum(diff ** 2, axis=0))
-
-        direction = np.zeros((2, height, width), dtype=np.float32)
-        direction[0, rev > 0] = np.divide(diff[0, rev > 0], dist[rev > 0])
-        direction[1, rev > 0] = np.divide(diff[1, rev > 0], dist[rev > 0])
-
-        direction[0] = direction[0] * (dilmask > 0)
-        direction[1] = direction[1] * (dilmask > 0)
-
-        flux = -1 * np.stack((direction[0], direction[1]))
-
-        dilmask = (dilmask > 0).astype(np.float32)
-        dilmask = dilmask[np.newaxis, ...]
-        print(image.shape,flux.shape,dilmask.shape)
-        return image, flux, dilmask
 
 
 class TestDataLayer(Dataset):

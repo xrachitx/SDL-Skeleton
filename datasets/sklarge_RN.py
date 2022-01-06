ad83270a7700f37e5040b3a407f9791261d872de
xrachitx <45661647+xrachitx@users.noreply.github.com>
2022-01-06 12:02:21 +0530

    Update sklarge_RN.py

--- datasets/sklarge_RN.py
+++ datasets/sklarge_RN.py
@@ -46,6 +46,61 @@ class TrainDataset(Dataset):
         targetImage = torch.Tensor(targetImage)
         return inputImage, targetImage
 
+def _collate_fn(batch):
+    
+    minibatch_size = len(batch)
+    size_x,size_y = 256
+    imgs = torch.zeros(minibatch_size, 3, size_x, size_y)
+    gts = torch.zeros(minibatch_size, size_x, size_y)
+    for x in range(minibatch_size):
+        sample = batch[x]
+        tensor = sample[0]
+        target = sample[1]
+        w_a,h_a = tensor.size
+        maxx = max(w_a,h_a)
+        cap = 256
+        extra = 0
+
+        if maxx<= cap:
+            new_w, new_h = w_a,h_a
+        else:
+            r = cap/maxx
+            new_w, new_h = int(w_a*r), int(h_a*r)
+            img_transform = transforms.Compose([transforms.ToPILImage(mode="RGB"),transforms.Resize((new_h, new_w)), transforms.ToTensor()])
+            gt_transform = transforms.Compose([transforms.ToPILImage(mode="L"),transforms.Resize((new_h, new_w)), transforms.ToTensor()])
+            tensor = img_transform(tensor)
+            target = img_transform(target)
+        if i <2:
+            p_up = 0
+            p_down = ((cap+extra)-new_h)
+        else:
+            p_down = 0
+            p_up = ((cap+extra)-new_h)
+
+        if i == 0 or i ==2:
+            p_left = 0
+            p_right = ((cap+extra)-new_w)
+        else:
+            p_right = 0
+            p_left = ((cap+extra)-new_w)
+            
+        tensor = torch.nn.functional.pad(tensor,(p_left,p_right,p_up,p_down),value=0)
+        target = torch.nn.functional.pad(target,(p_left,p_right,p_up,p_down),value=0)
+        target = torch.squeeze(target,axis=0)
+        imgs[x,:,:,:] = tensor
+        target[x,:,:] = target
+        
+    
+#     targets = torch.IntTensor(targets)
+    return imgs, target
+
+
+class ImageDataLoader(DataLoader):
+    def __init__(self, *args, **kwargs):
+        super(ImageDataLoader, self).__init__(*args, **kwargs)
+        self.collate_fn = _collate_fn
+ 
+    
 
 class TestDataset(Dataset):
     def __init__(self, fileNames, rootDir, transform=None):

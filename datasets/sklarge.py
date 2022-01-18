ebb98b08098eb7a7619e5374a7909a702015f107
xrachitx <45661647+xrachitx@users.noreply.github.com>
2022-01-18 23:28:15 +0530

    Update sklarge.py

--- datasets/sklarge.py
+++ datasets/sklarge.py
@@ -21,7 +21,8 @@ class TrainDataset(Dataset):
         inputName = os.path.join(self.rootDir, self.frame.iloc[idx, 0])
         targetName = os.path.join(self.rootDir, self.frame.iloc[idx, 1])
 
-        inputImage = cv2.imread(inputName)
+#         inputImage = cv2.imread(inputName)
+        inputImage = io.imread(inputName)
         if len(inputImage.shape) == 2:
             inputImage = inputImage[:, :, np.newaxis]
             inputImage = np.repeat(inputImage, 3, axis=-1)
@@ -35,7 +36,8 @@ class TrainDataset(Dataset):
             inputImage -= np.array([104.00699, 116.66877, 122.67892])
             inputImage = inputImage.transpose((2, 0, 1))
 
-        targetImage = cv2.imread(targetName,cv2.IMREAD_GRAYSCALE)
+        targetImage = io.imread(targetName)
+#         targetImage = cv2.imread(targetName,cv2.IMREAD_GRAYSCALE)
         if len(targetImage.shape) == 3:
             targetImage = targetImage[:, :, 0]
         targetImage = targetImage > 0.0
